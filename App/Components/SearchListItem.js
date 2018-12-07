04fb1b46d4dcbaa0f2ac77fad569ce68e8bc39f7
tuxfr <tuxfr@free.fr>
2018-12-07 18:43:33 +0100

    component SearchListItem

--- App/Components/SearchListItem.js
+++ App/Components/SearchListItem.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
   View,
-  Container,
-  Content,
   Card,
   CardItem,
   Thumbnail,
@@ -12,6 +10,7 @@ import {
   Right,
   Body,
 } from 'native-base'
+import { take, isEmpty, } from 'lodash'
 
 import styles from './Styles/SearchListItemStyle'
 
@@ -31,36 +30,49 @@ export default class SearchListItem extends Component {
   // Defaults for props
   static defaultProps = {
     year: '----',
-    genre: '',
-    style: null,
+    genre: [],
+    style: [],
     country: '',
     coverArtUrl: null,
     thumbUrl: null,
   }
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      genre: [],
+      style: [],
+    }
+  }
+
+  static getDerivedStateFromProps (nextProps, prevState) {
+    const { genre = [], style = [], } = nextProps
+    return {
+      genre: take(genre, 2),
+      style: take(style, 2),
+    }
+  }
+
   render () {
-    const { title, genre, style, thumbUrl, year, } = this.props
+    const { title, thumbUrl, year, } = this.props
+    const { genre, style, } = this.state
     return (
-      <Container>
-        <Content>
-          <Card>
-            <CardItem>
-              <Left>
-                {thumbUrl && <Thumbnail square source={{
-                  uri: thumbUrl
-                }}/>}
-                <Body>
-                <Text>{title}</Text>
-                <View style={styles.titleContainer}>
-                  <Left><Text note>{genre} {style && `/ ${style}`}</Text></Left>
-                  <Right><Text note style={styles.year}>{year}</Text></Right>
-                </View>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+      <Card>
+        <CardItem>
+          <Left>
+            {thumbUrl && <Thumbnail square source={{
+              uri: thumbUrl
+            }}/>}
+            <Body>
+            <Text>{title}</Text>
+            <View style={styles.titleContainer}>
+              <Left><Text note>{genre} {!isEmpty(style) && `/ ${style}`}</Text></Left>
+              <Right><Text note style={styles.year}>{year}</Text></Right>
+            </View>
+            </Body>
+          </Left>
+        </CardItem>
+      </Card>
     )
   }
 }
